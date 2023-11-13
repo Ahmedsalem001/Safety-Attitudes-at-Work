450f87aff3357e7e63599684fb609dee077865f1
Ahmedsalem001 <99257541+Ahmedsalem001@users.noreply.github.com>
2023-11-14 01:09:23 +0200

    ui update

---
+++ Safety-Attitudes-at-Work/Assets/Ahmed/volumeSettings.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class volumeSettings : MonoBehaviour
+{
+   [SerializeField] private AudioMixer myMixer;
+   [SerializeField] private Slider musicSlider;
+   public void Quit(){
+    Debug.Log("Quit");
+         Application.Quit();
+   }
+}
